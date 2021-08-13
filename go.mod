cdd9ae02d6e51fecbca019402d8237e8ce2aee3c
Tanya Verma <tverma@cloudflare.com>
2021-08-13 15:59:31 -0700

    first

---
+++ go.mod
@@ -0,0 +1,7 @@
+module consumer
+
+go 1.16
+
+require (
+	github.com/tanyav2/bigboss v0.4.0
+)
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tanyav2/bigboss"
+)
+
+func main() {
+	x := bigboss.AmIBigBoss()
+	fmt.Printf("i am big boss: %v\n", x)
+}
+
